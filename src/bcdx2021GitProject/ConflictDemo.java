496b77a811037c83b6b3fd4892ada955b2a10dcb
PC20193679\PC20193679 <anykomazaraki@gamil.com>
2021-07-26 23:15:56 +0900

    add file for demo

---
+++ src/bcdx2021GitProject/ConflictDemo.java
@@ -0,0 +1,18 @@
+package bcdx2021GitProject;
+
+public class ConflictDemo {
+
+	/*
+	 * return true:OKワード,false:NGワード
+	 * */
+	public static boolean checkNickName(String nickName){
+
+		boolean isOK = true;
+		//ここにNGワードを弾く処理を書く
+
+
+
+		return isOK;
+
+	}
+}
