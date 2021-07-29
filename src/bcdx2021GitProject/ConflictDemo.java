c6986e4297ef358a73abf8543d417590908b4580
hazukiyoshizawa <hazuki@mail.benesse.jp>
2021-07-29 16:23:37 +0900

    hazukiyoshizawa

--- src/bcdx2021GitProject/ConflictDemo.java
+++ src/bcdx2021GitProject/ConflictDemo.java
@@ -8,13 +8,8 @@ public class ConflictDemo {
 	public static boolean checkNickName(String nickName){
 
 		boolean isOK = true;
-		System.out.println("惇平いつもありがとう！");
-		System.out.println("じゅんぺいの優しさにいつも救われています！");
-		System.out.println("これからも同居人としてかなり濃いお付き合いになると思いますが、");
-		System.out.println("何卒よろしくお願い申し上げます。(#^^#)");
-		int tekitounahensuu = 3;
 		//ここにNGワードを弾く処理を書く
-
+		
 
 
 		return isOK;
--- src/bcdx2021GitProject/ValidateTest.java
+++ src/bcdx2021GitProject/ValidateTest.java
@@ -44,7 +44,12 @@ public class ValidateTest {
 
 		boolean isOK = true;
 		//ここにNGワードを弾く処理を書く
-
+		if(nickName.contains("090")) isOK = false;
+		if(nickName.contains("県")) isOK = false;
+		if(nickName.contains("都")) isOK = false;
+		if(nickName.contains("道")) isOK = false;
+		if(nickName.contains("府")) isOK = false;
+		if(nickName.contains("@")) isOK = false;
 
 
 		return isOK;
