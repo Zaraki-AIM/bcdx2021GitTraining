0bae7eba36072056c7561f3f12da8d7030089fae
hazukiyoshizawa <hazuki@mail.benesse.jp>
2021-07-29 15:10:13 +0900

    hazukiyoshizawa

--- src/bcdx2021GitProject/ConflictDemo.java
+++ src/bcdx2021GitProject/ConflictDemo.java
@@ -12,6 +12,7 @@ public class ConflictDemo {
 		System.out.println("じゅんぺいの優しさにいつも救われています！");
 		System.out.println("これからも同居人としてかなり濃いお付き合いになると思いますが、");
 		System.out.println("何卒よろしくお願い申し上げます。(#^^#)");
+		int tekitounahensuu = 3;
 		//ここにNGワードを弾く処理を書く
 
 
