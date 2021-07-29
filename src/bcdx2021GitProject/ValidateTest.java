bea57868f9851cdfc2908bb82a9ac59b67c2ce08
koba09yuta <koba09yuta@gmail.com>
2021-07-29 15:07:25 +0900

    Kobayashi

--- src/bcdx2021GitProject/ValidateTest.java
+++ src/bcdx2021GitProject/ValidateTest.java
@@ -28,7 +28,7 @@ public class ValidateTest {
 
             //結果表示
             System.out.println("NGワード：" + validateCount + "/" + totalCount + ":" + validateCount*100/totalCount +"%");
-            System.out.println("ssssss");
+            System.out.println("sssssssss");
         } catch (FileNotFoundException e){
             e.printStackTrace();
             System.exit(-1); // 0 以外は異常終了
