bbd67d0535ea8b5221825e2139faedc4a477beac
maru-00 <r_haya@mail.benesse.co.jp>
2021-07-29 16:20:26 +0900

    バリデート編集

--- src/bcdx2021GitProject/ValidateTest.java
+++ src/bcdx2021GitProject/ValidateTest.java
@@ -1,9 +1,41 @@
 package bcdx2021GitProject;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ValidateTest {
 
 	public static void main(String[] args)
 	{
+		int validateCount = 0;
+		int totalCount = 0;
+		String filename = "src/sample.txt";
+
+        try (BufferedReader in = new BufferedReader(new FileReader(new File(filename)))){
+            String line;
+            while((line = in.readLine()) != null){
+            	totalCount++;
+            	if(!checkNickName(line)){
+            		System.out.println(line + "：NG!");
+            		validateCount++;
+            	}else{
+            		System.out.println(line + "：OK!");
+            	}
+            }
+
+            //結果表示
+            System.out.println("NGワード：" + validateCount + "/" + totalCount + ":" + validateCount*100/totalCount +"%");
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+            System.exit(-1); // 0 以外は異常終了
+        } catch (IOException e){
+            e.printStackTrace();
+            System.exit(-1);
+        }
+
 
 	}
 
@@ -15,9 +47,47 @@ public class ValidateTest {
 
 		boolean isOK = true;
 		//ここにNGワードを弾く処理を書く
-
-		System.out.println("Hello World!");
-		System.out.println("Bye");
+		
+		/*空文字を弾く*/
+		if("".equals(nickName)) {
+			isOK = false;
+		}
+		
+		if(nickName.contains("-")) {
+			isOK = false;
+		}
+		
+		if(nickName.contains("－")) {
+			isOK = false;
+		}
+		
+		if(nickName.contains("@")) {
+			isOK = false;
+		}
+		
+		if(nickName.contains("都")) {
+			isOK = false;
+		}
+		
+		if(nickName.contains("道")) {
+			isOK = false;
+		}
+		
+		if(nickName.contains("府")) {
+			isOK = false;
+		}
+		
+		if(nickName.contains("県")) {
+			isOK = false;
+		}
+		
+		/*if(nickName.contains("")) {
+			isOK = false;
+		}*/
+		
+		if(nickName.length()>16) {
+			isOK = false;
+		}
 		
 		return isOK;
 	}
