12db643b0249d5ef1bdd7fb8344206856e184298
ando <miki.ando@mail.benesse.co.jp>
2021-07-29 16:27:47 +0900

    空文字_ando

--- src/bcdx2021GitProject/ValidateTest.java
+++ src/bcdx2021GitProject/ValidateTest.java
@@ -8,44 +8,60 @@ import java.io.IOException;
 
 public class ValidateTest {
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		int validateCount = 0;
 		int totalCount = 0;
 		String filename = "src/sample.txt";
 
-        try (BufferedReader in = new BufferedReader(new FileReader(new File(filename)))){
-            String line;
-            while((line = in.readLine()) != null){
-            	totalCount++;
-            	if(!checkNickName(line)){
-            		System.out.println(line + "：NG!");
-            		validateCount++;
-            	}else{
-            		System.out.println(line + "：OK!");
-            	}
-            }
+		try (BufferedReader in = new BufferedReader(new FileReader(new File(filename)))) {
+			String line;
+			while ((line = in.readLine()) != null) {
+				totalCount++;
+				if (!checkNickName(line)) {
+					System.out.println(line + "：NG!");
+					validateCount++;
+				} else {
+					System.out.println(line + "：OK!");
+				}
+			}
 
-            //結果表示
-            System.out.println("NGワード：" + validateCount + "/" + totalCount + ":" + validateCount*100/totalCount +"%");
-        } catch (FileNotFoundException e){
-            e.printStackTrace();
-            System.exit(-1); // 0 以外は異常終了
-        } catch (IOException e){
-            e.printStackTrace();
-            System.exit(-1);
-        }
+			//結果表示
+			System.out.println(
+					"NGワード：" + validateCount + "/" + totalCount + ":" + validateCount * 100 / totalCount + "%");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.exit(-1); // 0 以外は異常終了
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
 	}
 
 	/*
 	 * return true:OKワード,false:NGワード
 	 * */
-	public static boolean checkNickName(String nickName){
-
+	public static boolean checkNickName(String nickName) {
 		boolean isOK = true;
 		//ここにNGワードを弾く処理を書く
+		String[] strArray = nickName.split("");
+		for (int i = 0; i < strArray.length; i++) {
+		}
 
+		//空文字
+		if (nickName == "") {
+			isOK = false;
+		}
 
+		//文字が16文字以上
+		if (strArray.length > 16) {
+			isOK = false;
+		}
+		
+		/*//電話番号
+		for(int i=0; i<strArray.length; i++) {
+			char c = strArray[i].toCharArray();
+			if(strArray[i].)
+		}*/
 
 		return isOK;
 
