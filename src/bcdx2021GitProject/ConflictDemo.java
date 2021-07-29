ebaa125e1d58d8555ad8f1bb5610eadc07f96682
hazukiyoshizawa <hazuki@mail.benesse.jp>
2021-07-29 14:55:38 +0900

    hazukiyoshizawa

--- src/bcdx2021GitProject/ConflictDemo.java
+++ src/bcdx2021GitProject/ConflictDemo.java
@@ -8,6 +8,10 @@ public class ConflictDemo {
 	public static boolean checkNickName(String nickName){
 
 		boolean isOK = true;
+		System.out.println("惇平いつもありがとう！");
+		System.out.println("じゅんぺいの優しさにいつも救われています！");
+		System.out.println("これからも同居人としてかなり濃いお付き合いになると思いますが、");
+		System.out.println("何卒よろしくお願い申し上げます。(#^^#)");
 		//ここにNGワードを弾く処理を書く
 
 
