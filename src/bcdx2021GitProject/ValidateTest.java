041ca1e755a22cb475ae882846e4df00877f2fce
ando <miki.ando@mail.benesse.co.jp>
2021-07-29 14:59:34 +0900

    change_ando

--- src/bcdx2021GitProject/ValidateTest.java
+++ src/bcdx2021GitProject/ValidateTest.java
@@ -10,6 +10,9 @@ public class ValidateTest {
 
 	public static void main(String[] args)
 	{
+		System.out.println("開発が大変ー");
+		System.out.println("がんばりますー");
+		
 		int validateCount = 0;
 		int totalCount = 0;
 		String filename = "src/sample.txt";
