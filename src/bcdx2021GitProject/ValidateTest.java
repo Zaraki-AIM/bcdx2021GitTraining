4a6c921276cfc856e58aa76f4a4b55ac940b82e3
Zaraki-AIM <k-ymzk@mail.benesse.co.jp>
2022-06-20 18:14:34 +0900

    a

--- src/bcdx2021GitProject/ValidateTest.java
+++ src/bcdx2021GitProject/ValidateTest.java
@@ -44,8 +44,8 @@ public class ValidateTest {
 
 		boolean isOK = true;
 		//ここにNGワードを弾く処理を書く
-		String　nickName;
-		boolean isOK = false;
+		//String nickName;
+		//boolean isOK = false;
 
 
 		return isOK;
